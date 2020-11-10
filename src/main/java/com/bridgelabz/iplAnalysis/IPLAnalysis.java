6c10d967d5f577151246a4d85fe40767eeb88ee9
asmitamthorat <asmita26296@gmail.com>
2020-11-10 21:07:00 +0530

    ADDED:average of runs

--- src/main/java/com/bridgelabz/iplAnalysis/IPLAnalysis.java
+++ src/main/java/com/bridgelabz/iplAnalysis/IPLAnalysis.java
@@ -1,17 +1,20 @@
 package com.bridgelabz.iplAnalysis;
 
+import com.google.gson.Gson;
+
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Comparator;
 import java.util.List;
 
 public class IPLAnalysis {
-    public int loadCSVFile1(String iplData1) throws IPLAnalysisException {
+    public List<IPLData1> loadCSVFile1(String iplData1) throws IPLAnalysisException {
         try(Reader reader= Files.newBufferedReader(Paths.get(iplData1))){
             ICSVBuilder csvBuilder=CSVBuilderFactory.createCSVBuilder();
             List<IPLData1> IPLData1CsvList=csvBuilder.getCSVFileList(reader,IPLData1.class);
-            return IPLData1CsvList.size();
+            return IPLData1CsvList;
 
         }catch (IOException ioException){
             throw new IPLAnalysisException("File problem", IPLAnalysisException.ExceptionType.FILE_PROBLEM);
@@ -19,16 +22,61 @@ public class IPLAnalysis {
         }
     }
 
-    public int loadCSVFile2(String iplData2) throws IPLAnalysisException {
+    public List<IPLData2> loadCSVFile2(String iplData2) throws IPLAnalysisException {
         try(Reader reader= Files.newBufferedReader(Paths.get(iplData2))){
             ICSVBuilder csvBuilder=CSVBuilderFactory.createCSVBuilder();
             List<IPLData2> IPLData2CsvList=csvBuilder.getCSVFileList(reader,IPLData2.class);
-            System.out.println(IPLData2CsvList);
-            return IPLData2CsvList.size();
+
+            return IPLData2CsvList;
 
         }catch (IOException ioException){
             throw new IPLAnalysisException("File problem", IPLAnalysisException.ExceptionType.FILE_PROBLEM);
 
         }
     }
+
+
+
+
+
+
+
+
+      public int getBattingAvg(String iplData1) throws IPLAnalysisException {
+        int avgBattingScore=0;
+        int totalScore=0;
+          List<IPLData1> IPLData1CsvList= loadCSVFile1(iplData1);
+          for(int i=0;i<IPLData1CsvList.size();i++) {
+              IPLData1 data = IPLData1CsvList.get(i);
+              totalScore=totalScore+data.Runs;
+          }
+          avgBattingScore=totalScore/IPLData1CsvList.size();
+          return avgBattingScore;
+  }
+
+    public String getHighestStrikingRate_Player(String iplData2) throws IPLAnalysisException {
+        List<IPLData2> IPLData2CsvList= loadCSVFile2(iplData2);
+        Comparator<IPLData2> StateCodeCSVComparator=Comparator.comparing(census -> census.SR);
+       sort(IPLData2CsvList,StateCodeCSVComparator);
+        String sortedStateCensusJson=new Gson().toJson(IPLData2CsvList);
+        System.out.println(IPLData2CsvList);
+
+        return sortedStateCensusJson;
+    }
+
+    private <E> void sort(List<E> CsvList, Comparator<E> CSVComparator) {
+        for (int i = 0; i < CsvList.size() - 1; i++) {
+            for (int j = 0; j < CsvList.size() - i - 1; j++) {
+                E census1 = CsvList.get(j);
+                E census2 = CsvList.get(j + 1);
+                if (CSVComparator.compare(census1, census2) < 0) {
+                    CsvList.set(j, census2);
+                    CsvList.set(j + 1, census1);
+                }
+            }
+        }
+    }
+
+
+
 }
--- src/test/java/com/bridgelabz/iplAnalysis/IplAnalysisTest.java
+++ src/test/java/com/bridgelabz/iplAnalysis/IplAnalysisTest.java
@@ -1,7 +1,10 @@
 package com.bridgelabz.iplAnalysis;
 
+import com.google.gson.Gson;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.List;
 
 public class IplAnalysisTest {
      private static final String IPL_DATA1="./src/test/resources/IPL1.csv";
@@ -11,15 +14,34 @@ public class IplAnalysisTest {
     @Test
     public void givenIplData1_ifHasCorrectNumberOfRecord_ShouldReturnTrue() throws IPLAnalysisException {
         IPLAnalysis iplAnalysis=new IPLAnalysis();
-        int numberOfRecord=iplAnalysis.loadCSVFile1(IPL_DATA1);
-        Assert.assertEquals(101,numberOfRecord);
+        List<IPLData1> list=iplAnalysis.loadCSVFile1(IPL_DATA1);
+        Assert.assertEquals(101,list.size());
     }
 
     @Test
-    public void givenIPLData2_ifHasCorrectNumberOfRecord_ShouldReturn() throws IPLAnalysisException{
+    public void givenIPLData2_ifHasCorrectNumberOfRecord_ShouldReturnTrue() throws IPLAnalysisException{
         IPLAnalysis iplAnalysis=new IPLAnalysis();
-        int numberOfRecord=iplAnalysis.loadCSVFile2(IPL_DATA2);
-        Assert.assertEquals(99,numberOfRecord);
+        List<IPLData2> list=iplAnalysis.loadCSVFile2(IPL_DATA2);
+        Assert.assertEquals(99,list.size());
     }
+
+    @Test
+    public void battingAvgOfPayer_ifHasCorrectNumberOfRecord_ShouldReturnTrue() throws IPLAnalysisException{
+        IPLAnalysis iplAnalysis=new IPLAnalysis();
+        int battingAvg=iplAnalysis.getBattingAvg(IPL_DATA1);
+        Assert.assertEquals(185,battingAvg);
+
+    }
+
+    @Test
+    public void battingPayerWithHighestStrikeRte_ifHasCorrectNumberOfRecord_ShouldReturnTrue() throws IPLAnalysisException{
+        IPLAnalysis iplAnalysis=new IPLAnalysis();
+        String list=iplAnalysis.getHighestStrikingRate_Player(IPL_DATA2);
+       // IndiaCensusCSV[] censusCSV=new Gson().fromJson(sortedCensusData,IndiaCensusCSV[].class);
+      //  Assert.assertEquals(1.99812341E8 ,censusCSV[0].population,0.00001);
+
+    }
+
+
 
 }
